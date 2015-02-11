55d8054639eb402237f900c2bbdf10d8be69a475
David O' Rojo <david.orojo@gmail.com>
2015-02-10 18:09:23 -0600

    Extended AbstractProvider.

--- src/Volabit.php
+++ src/Volabit.php
@@ -1,6 +1,27 @@
+<?php namespace Volabit;
 
-namespace Volabit;
+use League\OAuth2\Client\Provider\AbstractProvider;
 
-class Client {
+class Client extends AbstractProvider {
+  const PRODUCTION_SITE = 'https://stageex.volabit.com';
+  const SANDBOX_SITE = 'https://sandbox.volabit.com';
 
+  public function urlAuthorize() {
+    return $this->siteFor('production').'/oauth/authorize';
+  }
+
+  public function urlAccessToken() {
+    return $this->siteFor('production').'/oauth/access_token';
+  }
+
+  public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token) {}
+  public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token) {}
+
+  private function siteFor($env) {
+    return ($env == 'production') ? $this::PRODUCTION_SITE : $this::SANDBOX_SITE;
+  }
+
+  public function __toString() {
+    return print_r($this, true);
+  }
 }
