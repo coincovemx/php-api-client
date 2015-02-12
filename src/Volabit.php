ee6c3c0a2ce961e07948227d771a36e25de33a1f
David O' Rojo <david.orojo@gmail.com>
2015-02-11 18:14:55 -0600

    Completed Auth module.

--- src/Volabit.php
+++ src/Volabit.php
@@ -1,24 +1,47 @@
 <?php namespace Volabit;
 
-use League\OAuth2\Client\Provider\AbstractProvider;
+require_once(dirname(__FILE__).'/Volabit/Auth.php');
 
-class Client extends AbstractProvider {
-  const PRODUCTION_SITE = 'https://stageex.volabit.com';
-  const SANDBOX_SITE = 'https://sandbox.volabit.com';
+use Auth\Manager;
 
-  public function urlAuthorize() {
-    return $this->siteFor('production').'/oauth/authorize';
+class Client {
+
+  public function __construct($id, $secret, $url, $env = 'production') {
+    $this->url = $url;
+    $config = $this->auth_params($id, $secret, $url, $env);
+    $this->authManager = new Auth\Manager($config);
   }
 
-  public function urlAccessToken() {
-    return $this->siteFor('production').'/oauth/token';
+  // OAuth2 ///////////////////////////////////////////////////////////
+
+  public function authorize() {
+    return $this->authManager->getAuthorizationUrl();
   }
 
-  public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token) {}
-  public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token) {}
+  public function getTokens($code) {
+    return $this->authManager->getAccessToken('authorization_code', [
+      'code' => $code, 'grant_type' => 'authorization_code'
+    ]);
+  }
 
-  private function siteFor($env) {
-    return ($env == 'production') ? $this::PRODUCTION_SITE : $this::SANDBOX_SITE;
+  public function refreshTokens($token) {
+    return $this->authManager->getAccessToken('refresh_token', [
+      'refresh_token' => $token, 'grant_type' => 'refresh_token'
+    ]);
+  }
+
+  // API Rates ////////////////////////////////////////////////////////
+  // API Users ////////////////////////////////////////////////////////
+  // API Slips ////////////////////////////////////////////////////////
+  // API Transactions /////////////////////////////////////////////////
+
+  private function auth_params($id, $secret, $url, $env) {
+    return [
+      'clientId' => $id,
+      'clientSecret' => $secret,
+      'redirectUri' => $url,
+      'env' => $env
+    ];
   }
 
   public function __toString() {
